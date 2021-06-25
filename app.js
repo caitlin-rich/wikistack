3e19c993977a94aeaba8de197fb24f478925038b
caitlin-rich <caitlinboagrich@gmail.com>
2021-06-25 13:00:56 -0400

    setup done

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const morgan = require('morgan')
 const express = require('express')
+const { db } = require('./models');
+
+const app = express();
 
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static((__dirname + 'public')));
 
 app.get('/', (req, res, next) => {
     res.send('Hello, world!')
 });
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database!');
+  })
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Listening at port ${PORT}! :)`)
+})
